60e1285f4945c56fc277489f82c9b1d7534a76e5
Shankha <sssanyal10@gmail.com>
2022-06-29 16:58:51 +0530

    Finished DP on Stocks

---
+++ Coding/src/dynamicprogramming/DP38_StockBuySellMultipleTimesAllowedWithCooldown.java
@@ -0,0 +1,131 @@
+package dynamicprogramming;
+
+import java.util.Arrays;
+
+/////////////////////////////////////////////////////////////////////////  RECURSIVE SOLUTION
+
+//public class DP38_StockBuySellMultipleTimesAllowedWithCooldown {
+//	public static void main(String args[]) {
+//
+//		int[] prices = { 7, 1, 5, 3, 6, 4 };
+//		System.out.println(stockProfit(prices));
+//	}
+//
+//	public static int stockProfit(int[] prices) {
+//		int buyFlag = 1;
+//		return maximizeProfit(prices, 0, buyFlag);
+//	}
+//
+//	private static int maximizeProfit(int[] prices, int index, int buyFlag) {
+//		
+//		if(index == prices.length)return 0;
+//		
+//		int profit = 0;
+//		
+//		if(buyFlag == 1) {
+//			profit = Math.max(-prices[index] + maximizeProfit(prices, index + 1, buyFlag^1),
+//					 0 + maximizeProfit(prices, index + 1, buyFlag));
+//		}
+//		else {
+//			//since cooldown period as per question is 2, so after sell we will go to index of i+2th stock
+//			profit = Math.max(prices[index] + maximizeProfit(prices, index + 2, buyFlag^1),
+//					 0 + maximizeProfit(prices, index + 1, buyFlag));
+//		}
+//		
+//		
+//		return profit;
+//	}
+//}
+
+
+//////////////////////////////////////////////////////////////  MEMOIZED SOLUTION
+
+//public class DP38_StockBuySellMultipleTimesAllowedWithCooldown {
+//	public static void main(String args[]) {
+//
+//		int[] prices = { 7, 1, 5, 3, 6, 4 };
+//		System.out.println(stockProfit(prices));
+//	}
+//
+//	public static int stockProfit(int[] prices) {
+//		int buyFlag = 1;
+//		int[][] dp = new int[prices.length][2];
+//		for(int[] ar : dp)Arrays.fill(ar, -1);
+//		
+//		return maximizeProfit(prices, 0, buyFlag, dp);
+//	}
+//
+//	private static int maximizeProfit(int[] prices, int index, int buyFlag, int[][] dp) {
+//		
+//		if(index >= prices.length)return 0;
+//		
+//		int profit = 0;
+//		
+//		if(dp[index][buyFlag] != -1)return dp[index][buyFlag];
+//		
+//		if(buyFlag == 1) {
+//			profit = Math.max(-prices[index] + maximizeProfit(prices, index + 1, buyFlag^1, dp),
+//					 0 + maximizeProfit(prices, index + 1, buyFlag, dp));
+//		}
+//		else {
+//			//since cooldown period as per question is 2, so after sell we will go to index of i+2th stock
+//			profit = Math.max(prices[index] + maximizeProfit(prices, index + 2, buyFlag^1, dp),
+//					 0 + maximizeProfit(prices, index + 1, buyFlag, dp));
+//		}
+//		
+//		
+//		return dp[index][buyFlag] = profit;
+//	}
+//}
+
+//////////////////////////////////////////////////////////////////////  TABULAR SOLUTION
+
+
+public class DP38_StockBuySellMultipleTimesAllowedWithCooldown {
+	public static void main(String args[]) {
+
+		int[] prices = { 7, 1, 5, 3, 6, 4 };
+		System.out.println(stockProfit(prices));
+	}
+
+	public static int stockProfit(int[] prices) {
+		int buyFlag = 1;
+		int[][] dp = new int[prices.length+2][2];
+		
+		return maximizeProfit(prices, dp);
+	}
+
+	private static int maximizeProfit(int[] prices, int[][] dp) {
+		
+		
+		int profit = 0;
+		int n = prices.length;
+		
+		for(int index = n-1; index >= 0; index--) {
+			for(int buyFlag = 1; buyFlag >= 0; buyFlag--) {
+				
+				if(buyFlag == 1) {
+					profit = Math.max(-prices[index] + dp[index + 1][buyFlag^1],
+							 0 + dp[index + 1][buyFlag]);
+				}
+				else {
+					//since cooldown period as per question is 2, so after sell we will go to index of i+2th stock
+					profit = Math.max(prices[index] + dp[index + 2][buyFlag^1],
+							 0 + dp[index + 1][buyFlag]);
+				}
+				dp[index][buyFlag] = profit;
+			}
+		}
+		
+		
+		return dp[0][1];
+	}
+}
+
+
+
+
+
+
+
+
---
+++ Coding/src/dynamicprogramming/DP39_StockBuySellWithTransactionFee.java
@@ -0,0 +1,46 @@
+package dynamicprogramming;
+
+// Just copy pasted the buy & sell stock multiple time s allowed and deducted the fee at the sell phase 
+
+public class DP39_StockBuySellWithTransactionFee {
+	public static void main(String args[]) {
+
+		int[] prices = {1,3,2,8,4,9};
+		int fee = 2;
+		System.out.println(stockProfit(prices.length, fee, prices));
+	}
+
+	public static int stockProfit(int n, int fee, int[] prices) {
+		int buyFlag = 1;
+		int[][] dp = new int[prices.length+2][2];
+		
+		return maximizeProfit(prices, fee,  dp);
+	}
+
+	private static int maximizeProfit(int[] prices, int fee, int[][] dp) {
+		
+		
+		int profit = 0;
+		int n = prices.length;
+		
+		for(int index = n-1; index >= 0; index--) {
+			for(int buyFlag = 1; buyFlag >= 0; buyFlag--) {
+				
+				if(buyFlag == 1) {
+					profit = Math.max(-prices[index] + dp[index + 1][buyFlag^1],
+							 0 + dp[index + 1][buyFlag]);
+				}
+				else {
+					//since cooldown period as per question is 2, so after sell we will go to index of i+2th stock
+					//just deducted the fee while the sale is being made
+					profit = Math.max(prices[index] -fee  + dp[index + 1][buyFlag^1],
+							 0 + dp[index + 1][buyFlag]);
+				}
+				dp[index][buyFlag] = profit;
+			}
+		}
+		
+		
+		return dp[0][1];
+	}
+}
